10109f2fe1320d550eaa996c73cac6bfe284ddda
adityabanik2004 <adityabanik2004@gmail.com>
2026-02-05 12:12:04 +0530

    Add files via upload

---
+++ chatbot/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_message = request.json["message"]
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "user", "content": user_message}
+        ]
+    )
+
+    bot_reply = response.choices[0].message.content
+    return jsonify({"reply": bot_reply})
+
+if __name__ == "__main__":
+    app.run(debug=True)
